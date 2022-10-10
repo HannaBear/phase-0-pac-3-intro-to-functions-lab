726f649a6ba932a4690660407fbd7570bea86e10
Hanna <marmalademarmoset@gmail.com>
2022-10-10 12:47:28 -0700

    commit tree e9683f6bd7261c5f926e7b1f41a528d12527cac8

---
+++ index.js
@@ -0,0 +1,43 @@
+const string = "Hello";
+
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+    return string.toLowerCase();
+  }
+
+function logShout(string) {
+console.log(string.toUpperCase());
+  return string.toUpperCase();
+ }
+ 
+ function sayHiToHeadphonedRoommate(string) {
+    console.log(string.toLowerCase());
+      return string.toLowerCase();
+     }
+     
+function logWhisper(string) {
+        console.log(string.toLowerCase());
+          return string.toLowerCase();
+         }   
+         
+function sayHiToHeadphonedRoommate(string) {
+            if (string==="Let's have dinner together!")
+            return ("I would love to!");
+
+            else if (string=== string.toLowerCase())
+            return ("I can't hear you!");
+
+            else if (string=== string.toUpperCase())
+            return ("YES INDEED!");
+             }
+
+console.log(shout(string))
+console.log(whisper(string))
+console.log(logShout(string))
+console.log(sayHiToHeadphonedRoommate(string))
+console.log(logWhisper(string))
+
+             
